1cb4a0be172c8f54adf8f3d02d6c4d4b75eea7f8
sneh <sneh.codezee@gmail.com>
2024-09-15 15:47:42 +0530

    learing reactjs

---
+++ OneDrive/Desktop/react learing/day1/day1/src/App.js
@@ -0,0 +1,18 @@
+import './hari.css';
+
+function App() {
+  return (
+   <>
+    <div className="nav">
+          <div id="companylogo">HI</div>
+        <ul className="menu">
+          <li id="Gallery"><a href="/">Gallery</a></li>
+          <li className="Contact"><a href="/">Contact</a></li>
+          <li className="About"><a href="/">About</a></li>
+        </ul>
+    </div>
+   </>
+  );
+}
+
+export default App;
